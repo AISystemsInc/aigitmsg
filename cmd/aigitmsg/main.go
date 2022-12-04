fcdee649e216f96725b79ff418354d19ade458b0
Bill Nunney <bill@bigtallbill.com>
2022-12-03 22:58:45 -0500

    Refactor: Update error handling for git diff command
    - Updated error handling for git diff command to provide better context

--- cmd/aigitmsg/main.go
+++ cmd/aigitmsg/main.go
@@ -17,7 +17,7 @@ import (
 func getGitDiff() (string, error) {
 	out, err := exec.Command("git", "diff", "--cached", "--no-prefix", "-U20").Output()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("git diff failed: %s", err)
 	}
 	return string(out), nil
 }
@@ -25,7 +25,7 @@ func getGitDiff() (string, error) {
 func getGitBranch() (string, error) {
 	out, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("git rev-parse failed: %s", err)
 	}
 	return string(out), nil
 }
@@ -33,7 +33,8 @@ func getGitBranch() (string, error) {
 func getGitTemplate() (string, error) {
 	out, err := exec.Command("git", "config", "--get", "commit.template").Output()
 	if err != nil {
-		return "", err
+		log.Printf("could not get git commit template: %s", err)
+		return "", nil
 	}
 	filename := strings.TrimSpace(string(out))
 	if filename == "" {
@@ -41,10 +42,13 @@ func getGitTemplate() (string, error) {
 	}
 	templateBytes, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to read git template file: %s", err)
 	}
 	return string(templateBytes), nil
 }
+
+const maxCompletionTokens = 500
+const maxAllowedTokens = 4097
 
 func main() {
 	gptAPIKey := flag.String("gpt-key", os.Getenv("GPT_API_KEY"), "GPT API Key")
@@ -74,7 +78,7 @@ func main() {
 
 	req := gogpt.CompletionRequest{
 		Model:       "text-davinci-003",
-		MaxTokens:   500,
+		MaxTokens:   maxCompletionTokens,
 		Prompt:      prompt,
 		Temperature: 0.7,
 	}
@@ -100,8 +104,7 @@ func buildPrompt(gitDiff, gitBranch, gitTemplate string) string {
 	if gitTemplate != "" {
 		prompt += gitTemplate
 	} else {
-		prompt += `The first line should start with an imperative word (e.g. Add, Remove, Refactor, etc.) followed by a colon and then be followed by a summary of the changes. A blank line should be next. The 3rd line should contain either a bulleted list of changes made or a more detail description of the changes as a whole.
-
+		prompt += `The first line should start with an imperative word (e.g. Add, Remove, Refactor, etc.) followed by a colon and then be followed by a summary of the changes. A blank line should be next. The 3rd line should contain either a bulleted list of the general idea of changes made and/or a more detail description of the changes as a whole. The 4th line should be omitted if the branch name is a standard git branch (like main or master), otherwise if the branch name looks like a task/issue/ticket ID then add a line with "Relates to: BranchName"
 `
 	}
 
@@ -111,6 +114,11 @@ func buildPrompt(gitDiff, gitBranch, gitTemplate string) string {
 	if gitBranch != "" {
 		prompt += `
 The branch name is ` + gitBranch
+	}
+
+	if len(prompt)+len(gitDiff) > maxAllowedTokens {
+		log.Printf("prompt is too long, truncating to %d characters", maxAllowedTokens-len(gitDiff))
+		gitDiff = gitDiff[:maxAllowedTokens-len(prompt)]
 	}
 
 	if gitDiff != "" {
